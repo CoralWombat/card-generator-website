e463fdae93084fc345164a8af8dee779fbb2f618
Kristóf Göncző <kristofgonczo@gmail.com>
2024-02-24 21:55:53 +0100

    Working around font not loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { Button } from "primereact/button";
-import { PrimeReactProvider } from "primereact/api";
 import "./App.scss";
 
 const App = () => {
   return (
-    <PrimeReactProvider>
+    <div>
       <h1>Hello</h1>
-      <div className="card flex justify-content-center">
-        <Button label="Check" icon="pi pi-check" />
-      </div>
-    </PrimeReactProvider>
+      <Button label="Click" icon="pi pi-plus"></Button>
+    </div>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import App from "./App";
 import "./App.scss";
 
 import { createRoot } from "react-dom/client";
-const container = document.getElementById("app");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(
   <React.StrictMode>
     <App />
