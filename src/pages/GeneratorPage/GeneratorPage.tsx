7b13b0c45390f4288a963035279fd899a8272a4b
Kristóf Göncző <kristofgonczo@gmail.com>
2024-02-25 14:30:37 +0100

    GeneratorPage

--- src/pages/GeneratorPage/GeneratorPage.tsx
+++ src/pages/GeneratorPage/GeneratorPage.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import { GeneratorMode } from "../../model/GeneratorMode";
+import { InputSwitch } from "primereact/inputswitch";
+import { Dropdown } from "primereact/dropdown";
+import TemplateRenderer from "../../components/TemplateRenderer/TemplateRenderer";
+
 const GeneratorPage = () => {
-  return <div>GeneratorPage placeholder</div>;
+  const [mode, setMode] = useState(GeneratorMode.Basic);
+  const [template, setTemplate] = useState<string>(null);
+
+  useEffect(() => {
+    console.log("Mode changed: ", mode);
+  }, [mode]);
+
+  return (
+    <div className="grid">
+      <div className="col flex flex-column gap-1">
+        <InputSwitch
+          checked={mode === GeneratorMode.Advanced}
+          onChange={(e: { value: any }) => {
+            setMode(e.value ? GeneratorMode.Advanced : GeneratorMode.Basic);
+          }}
+        />
+        <Dropdown
+          value={template}
+          onChange={(e) => setTemplate(e.value)}
+          options={[
+            {
+              name: "Basic Template",
+              value: "src/templates/basic-template.html",
+            },
+          ]}
+          optionLabel="name"
+          placeholder="Select Template"
+        />
+      </div>
+      <div className="col flex justify-content-center align-content-center flex-wrap">
+        <TemplateRenderer template={template} />
+      </div>
+    </div>
+  );
 };
 
 export default GeneratorPage;
