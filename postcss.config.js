0e5819949e47537e699a2fac2fff3d5643938f5f
Kristóf Göncző <kristofgonczo@gmail.com>
2024-03-07 10:57:14 +0100

    Moving tailwind to webpack config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  plugins: [require("postcss-preset-env")],
+  plugins: [
+    require("postcss-preset-env"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("cssnano"),
+  ],
 };
