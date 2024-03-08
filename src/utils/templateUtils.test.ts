9fd0cd04861648b7dcb736a23a952f564fc20b45
Kristóf Göncző <kristofgonczo@gmail.com>
2024-03-08 08:51:54 +0100

    Testing template utils

---
+++ src/utils/templateUtils.test.ts
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import { getTemplateByName, getTemplateParameterNames } from "./templateUtils";
+
+it("gets the template parameter names from template", async () => {
+    const param0 = "PARAM_1";
+    const param1 = "PARAM_2";
+
+    const template = ["{{", param0, "}} is param1 and ", "{{&", param1, "}}."].join("");
+
+    const templateParams = getTemplateParameterNames(template);
+
+    expect(templateParams.length).toBe(2);
+    expect(templateParams[0]).toBe(param0);
+    expect(templateParams[1]).toBe(param1);
+});
