1de2477d9f9bf353ad47ef046ad91e37d8a07047
Kristóf Göncző <kristofgonczo@gmail.com>
2024-03-12 14:48:08 +0100

    Adding HowToPage tests

--- src/pages/HowToPage/HowToPage.test.tsx
+++ src/pages/HowToPage/HowToPage.test.tsx
@@ -1,4 +1,4 @@
-import { screen, render, cleanup } from "@testing-library/react";
+import { screen, render, cleanup, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import HowToPage from "./HowToPage";
 
@@ -7,5 +7,19 @@ afterEach(cleanup);
 it("renders how-to page", async () => {
   render(<HowToPage />);
 
-  expect(screen.getByText(/The basic mode/i)).toBeInTheDocument();
+  expect(screen.getAllByText(/Basic Usage/).length).toBe(2);
+});
+
+it("navigates to Uploading Own Data section", async () => {
+  const page = render(<HowToPage />);
+  fireEvent.click(page.getByText(/Uploading Own Data/));
+
+  expect(page.getAllByText(/Uploading Own Data/).length).toBe(2);
+});
+
+it("navigates to Create Your Own Template section", async () => {
+  const page = render(<HowToPage />);
+  fireEvent.click(page.getByText(/Create Your Own Template/));
+
+  expect(page.getAllByText(/Create Your Own Template/).length).toBe(2);
 });
