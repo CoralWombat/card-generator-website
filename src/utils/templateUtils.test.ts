eda4b18e9d104c93f3e56a33d2b72c80912b5d3f
Kristóf Göncző <kristofgonczo@gmail.com>
2024-03-21 20:13:11 +0100

    Testing getTemplateParameterNames (ci skip)

--- src/utils/templateUtils.test.ts
+++ src/utils/templateUtils.test.ts
@@ -13,3 +13,16 @@ it("gets the template parameter names from template", async () => {
     expect(templateParams[0]).toBe(param0);
     expect(templateParams[1]).toBe(param1);
 });
+
+it("removes parameters that are used twice", async () => {
+    const param0 = "PARAM_1";
+    const param1 = "PARAM_2";
+
+    const template = ["{{", param0, "}} is param1 and {{", param0, "}} is also param1, but {{&", param1, "}}."].join("");
+
+    const templateParams = getTemplateParameterNames(template);
+
+    expect(templateParams.length).toBe(2);
+    expect(templateParams[0]).toBe(param0);
+    expect(templateParams[1]).toBe(param1);
+});
