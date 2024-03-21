dd0c3fd44e4bf3a1d62646b6f36eef27d2886d87
Kristóf Göncző <kristofgonczo@gmail.com>
2024-03-21 20:07:12 +0100

    Adding missing images (#minor)

--- src/components/constants/TemplateOptions.ts
+++ src/components/constants/TemplateOptions.ts
@@ -1,6 +1,8 @@
 import TemplateOption from "../../model/TemplateOption";
 import exploding_cats_icon from 'Pictures/exploding_cats_icon.png';
 import exploding_cats_image from 'Pictures/exploding_cats_image.jpg';
+import basic_image from 'Pictures/basic_image.jpg';
+import dnd_spell_card_image from 'Pictures/dnd_spell_card_image.jpg';
 
 export const customTemplateOption: TemplateOption = {
     label: "Custom Template",
@@ -13,8 +15,7 @@ const templateOptions: TemplateOption[] = [
         label: "Basic Template",
         name: "basic-template.html",
         defaultValues: {
-            Image_Source:
-                "https://th.bing.com/th/id/OIG1.D7RIkM1p994fC1T1qVE8?w=1024&h=1024&rs=1&pid=ImgDetMain",
+            Image_Source: basic_image,
             Title: "Soft Kitty",
             Description:
                 "A soft kitty is a gentle and plush feline, often characterized by its fluffy fur and soothing purr.",
@@ -52,8 +53,7 @@ const templateOptions: TemplateOption[] = [
         label: "D&D Spell Card",
         name: "dnd_spell_card_template.html",
         defaultValues: {
-            Image_Source:
-                "https://th.bing.com/th/id/OIG1.0LHNGA6aZmQso9rp9UVy?pid=ImgGn",
+            Image_Source: dnd_spell_card_image,
             Title: "Mage Armor",
             Level: "1st",
             Casting_Time: "1 Action",
